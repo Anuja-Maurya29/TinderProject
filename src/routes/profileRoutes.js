d51a45defc34e367414660d133106dc8f4422a30
Anuja-Maurya29 <anumaurya@bestpeers.in>
2025-12-02 13:18:46 +0530

    cleaning repo

--- src/routes/profileRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import profileController from '../controllers/ProfileController.js'
-
-const profileRouter = express.Router();
-profileRouter.get('/getProfile',profileController.getProfile)
-profileRouter.patch('/createProfile',profileController.createProfile)
-
-export default profileRouter
--- src/service/generateToken.js
+++
@@ -1,7 +0,0 @@
-// jwt token
-
-import jwt from 'jsonwebtoken'
-
- export const generateToken =(id)=>{
-    return jwt.sign({id}, process.env.SECRET,{expiresIn:'7d'})
-}
